3b841313c597e689bd9b7be59c5c54e5efbd05dd
mardsonferreira <mardsonferreira25@gmail.com>
2021-10-15 08:21:56 -0300

    Creating header and index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { GetStaticProps } from 'next';
 
+import Head from 'next/head';
+
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
+
 import styles from './home.module.scss';
 
 interface Post {
@@ -25,7 +28,44 @@ interface HomeProps {
 }
 
 export default function Home(): JSX.Element {
-  return <h1>Hi there</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div>
+              <time>15 Mar 2021</time>
+              <span>Joseph Oliveira</span>
+            </div>
+          </a>
+
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div>
+              <time>15 Mar 2021</time>
+              <span>Joseph Oliveira</span>
+            </div>
+          </a>
+
+          <a>
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida</p>
+            <div>
+              <time>15 Mar 2021</time>
+              <span>Joseph Oliveira</span>
+            </div>
+          </a>
+        </div>
+      </main>
+    </>
+  );
 }
 
 // export const getStaticProps = async () => {
