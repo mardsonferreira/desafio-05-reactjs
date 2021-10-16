70ba941ac66febe45231d34e9e69ccac6896efd6
mardsonferreira <mardsonferreira25@gmail.com>
2021-10-16 09:26:10 -0300

    Getting posts from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from 'next';
-
 import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -27,7 +29,7 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
   return (
     <>
       <Head>
@@ -36,41 +38,59 @@ export default function Home(): JSX.Element {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida</p>
-            <div>
-              <time>15 Mar 2021</time>
-              <span>Joseph Oliveira</span>
-            </div>
-          </a>
-
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida</p>
-            <div>
-              <time>15 Mar 2021</time>
-              <span>Joseph Oliveira</span>
-            </div>
-          </a>
-
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida</p>
-            <div>
-              <time>15 Mar 2021</time>
-              <span>Joseph Oliveira</span>
-            </div>
-          </a>
+          {postsPagination.results.map(post => (
+            <a key={post.uid}>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div className={styles.postInfo}>
+                <div>
+                  <FiCalendar size={22} />
+                  <time>{post.first_publication_date}</time>
+                </div>
+                <div>
+                  <FiUser size={22} />
+                  <span>{post.data.author}</span>
+                </div>
+              </div>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['title', 'subtitle', 'author'],
+      pageSize: 100,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: RichText.asText(post.data.subtitle),
+        author: RichText.asText(post.data.author),
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: 2,
+    results: posts,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
